3c3bcbf90288dc116206dcf0123fe45839ea46ef
Andrej Bajalski <andrejbajalski@gmail.com>
2025-12-19 12:27:33 +0100

    successfully orchestrated containers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,8 +3,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Load .env file explicitly
 load_dotenv(BASE_DIR / ".env")
 
@@ -79,7 +78,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'NAME': BASE_DIR_BACKEND / 'db.sqlite3',
 #     }
 # }
 DATABASES = {
@@ -88,8 +87,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
