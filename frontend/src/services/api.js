1e232a21ab8a03f7ee63ff89978831063ecea395
Andrej Bajalski <andrejbajalski@gmail.com>
2025-12-26 16:38:55 +0100

    changed backend url paths

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 class ApiService {
-    constructor(baseURL="http://localhost:8000/api"){
+    constructor(baseURL="http://localhost:80/backend/api"){
         this.baseURL = baseURL
     }
     
