e2f67a85370e7cc88843eb9cb689ebd6fecbefa3
Andrej Bajalski <andrejbajalski@gmail.com>
2025-12-28 10:20:13 +0100

    testing api request to server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
 class ApiService {
-    constructor(baseURL="/backend/api"){
+    constructor(baseURL="http://backend-service.default.svc.cluster.local:8000/backend/api"){
         this.baseURL = baseURL
     }
     
     async get(endpoint, params = {}) {
-        return axios.get(`${this.baseURL}${endpoint}`)
+        const url = this.baseURL + endpoint
+        console.log('SENDING API REQUEST TO URL PATH: ', url)
+        return axios.get(url)
                     .then(response => response.data)
                     .catch(error => console.log('Error fetching data from API:', error ))
     }
     async getInternships(params = {}) {
-        return axios.get(`${this.baseURL}/internships/`)
+        const url = this.baseURL + "/internships"
+        console.log('SENDING API REQUEST TO URL PATH: ', url)
+        return axios.get(url)
                     .then(response => response.data)
                     .catch(error => console.log('Error fetching data from API:', error ))
     }
